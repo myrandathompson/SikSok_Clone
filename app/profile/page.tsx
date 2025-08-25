750e8cf0381379e7c5901a5906c33f3d4ef529d2
myrandathompson <myrandathompson@icloud.com>
2025-08-25 00:07:09 -0700

    a

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -83,7 +83,6 @@ export default function Profile({ params }: ProfilePageTypes) {
 
                     <ClientOnly>
                         <div className="mt-4 grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3">
-                            
                         </div>
                     </ClientOnly>
                 </div>
